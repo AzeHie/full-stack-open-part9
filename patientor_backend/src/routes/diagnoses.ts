ae3283be2e9a6a3423440d36954d3887b2859528
AzeHie <109618459+AzeHie@users.noreply.github.com>
2023-12-20 16:12:56 +0200

    Added some error handling

--- patientor_backend/src/routes/diagnoses.ts
+++ patientor_backend/src/routes/diagnoses.ts
@@ -4,9 +4,17 @@ import DiagnoseService from '../services/diagnoseService';
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-  const diagnosesData = DiagnoseService.getDiagnoses();
-
-  res.status(200).json(diagnosesData);
+  try {
+    const diagnosesData = DiagnoseService.getDiagnoses();
+  
+    res.status(200).json(diagnosesData);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).json(errorMessage);
+  }
 })
 
 export default router;
--- patientor_backend/src/routes/patients.ts
+++ patientor_backend/src/routes/patients.ts
@@ -4,22 +4,40 @@ import patientService from '../services/patientService';
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-  const NonSensitivePatientDetails = patientService.getNonSensitivePatientDetails();
 
-  res.status(200).json(NonSensitivePatientDetails);
+  try {
+    const NonSensitivePatientDetails =
+      patientService.getNonSensitivePatientDetails();
+  
+    res.status(200).json(NonSensitivePatientDetails);
+  } catch (error) {
+    let errorMessage = 'Something went wrong';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+  }
 });
 
 router.post('/', (req, res) => {
   const { name, dateOfBirth, ssn, gender, occupation } = req.body;
-  const addedPatient = patientService.addPatient({
-    name,
-    dateOfBirth,
-    ssn,
-    gender,
-    occupation
-  });
 
-  res.status(200).json(addedPatient);
-})
+  try {
+    const addedPatient = patientService.addPatient({
+      name,
+      dateOfBirth,
+      ssn,
+      gender,
+      occupation,
+    });
+  
+    res.status(200).json(addedPatient);
+  } catch (error) {
+    let errorMessage = 'Failed to add new patient';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(500).json(errorMessage);
+  }
+});
 
 export default router;
