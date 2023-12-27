00e618614a1e8bc42938bf457e92db4574105a54
AzeHie <109618459+AzeHie@users.noreply.github.com>
2023-12-27 15:56:12 +0200

    Finished exercise 9.16

--- flight_diary_frontend/src/App.tsx
+++ flight_diary_frontend/src/App.tsx
@@ -1,28 +1,36 @@
 import axios from 'axios';
 import { Fragment, useEffect, useState } from 'react';
-import './App.css'
+import './App.css';
 import { DiaryEntry } from './utils/Types';
 import Diary from './components/Diary';
 
 const App = () => {
   const [diary, setDiary] = useState<DiaryEntry[]>([]);
 
-
   useEffect(() => {
     const fetchDiary = async () => {
-      const result = await axios.get<DiaryEntry[]>(`http://localhost:3000/api/diaries`);
+      try {
+        const result = await axios.get<DiaryEntry[]>(
+          `http://localhost:3000/api/diaries`
+        );
 
-      setDiary(result.data);
+        const sortedData = result.data.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+        console.log(sortedData);
+        setDiary(result.data);
+      } catch (err) {
+        console.log(err);
+        throw new Error('Something went wrong!');
+      }
     };
 
     fetchDiary();
-  }, [])
+  }, []);
 
   return (
     <Fragment>
       <Diary diaryEntries={diary} />
     </Fragment>
-  )
-}
+  );
+};
 
 export default App;
--- flight_diary_frontend/src/components/Diary.tsx
+++ flight_diary_frontend/src/components/Diary.tsx
@@ -1,6 +1,8 @@
 import { Fragment } from 'react';
 import { DiaryEntry } from '../utils/Types';
 
+import './Diary.css';
+
 interface DiaryProps {
   diaryEntries: DiaryEntry[];
 }
@@ -8,13 +10,25 @@ interface DiaryProps {
 const Diary = (props: DiaryProps) => {
   return (
     <Fragment>
-      {props.diaryEntries.map((e) => (
-        <div key={e.id}>
-          {e.date}
-          {e.visibility}
-          {e.weather}
-        </div>
-      ))}
+      <h1>Flight diary</h1>
+      <table className='diary__table'>
+        <thead className='diary__thead'>
+          <tr>
+            <th>Date</th>
+            <th>Visibility</th>
+            <th>Weather</th>
+          </tr>
+        </thead>
+        <tbody className='diary__tbody'>
+          {props.diaryEntries.map((e) => (
+            <tr key={e.id}>
+              <td>{e.date}</td>
+              <td>{e.visibility}</td>
+              <td>{e.weather}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Fragment>
   );
 };
