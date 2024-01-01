12e1af4db3e1bead77e87f58233bc0a7640aa395
AzeHie <109618459+AzeHie@users.noreply.github.com>
2024-01-01 18:05:09 +0200

    Finished exercise 9.22

--- patientor_backend/src/util/types.ts
+++ patientor_backend/src/util/types.ts
@@ -4,9 +4,46 @@ export interface Diagnosis {
   latin?: string;
 }
 
-export interface Entry {
-
+export interface BaseEntry {
+  id: string;
+  date: string;
+  type: string;
+  specialist: string;
+  description: string;
+  diagnosisCodes?: Array<Diagnosis['code']>
 }
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  }
+}
+
+export interface HospitalEntry extends BaseEntry {
+  discharge: {
+    date: string;
+    criteria: string;
+  }
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry = 
+| HospitalEntry
+| OccupationalHealthcareEntry
+| HealthCheckEntry
 
 export interface Patient {
   id: string;
